17a0ddea176bbb78d81be661946eeb9a00f8d355
DESKTOP-8QPJ94B\user1 <sotnddu4653@gmail.com>
2026-08-31 11:05:26 +0900

    모델 학습 후 상관관계 히트맵을 통해 각 칼럼과 주택 가격의 관계 재확인

--- main.py
+++ main.py
@@ -79,6 +79,16 @@ df.to_csv("california_housing.csv")
 # plt.tight_layout()
 # plt.show()
 
+plt.figure(figsize=(10, 8))
+
+corr = df.corr(numeric_only=True)
+
+sns.heatmap(corr, annot=True, fmt=".2f", cmap="coolwarm")
+
+plt.title("칼럼별 상관관계")
+plt.tight_layout()
+plt.show()
+
 # 데이터 학습 준비
 features = ["MedInc", "AveRooms", "Latitude", "Longitude"]
 
